c0c2786c940cf7ca6ffa8ce1ed53a36757a275d7
김예안 <kjan0929@naver.com>
2025-05-21 00:06:36 +0900

    style: Footer ui 구현

--- MERN/frontend/src/App.jsx
+++ MERN/frontend/src/App.jsx
@@ -1,11 +1,13 @@
 import { BrowserRouter } from "react-router-dom";
 import "./App.css";
 import Navbar from "./Components/Navbar/Navbar";
+import Footer from "./Components/Footer/Footer";
 
 function App() {
   return (
     <BrowserRouter>
       <Navbar />
+      <Footer />
     </BrowserRouter>
   );
 }
