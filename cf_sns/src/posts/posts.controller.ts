c229f8c90aab6bfbfec3e9cafa207d9db8082750
김예안 <kjan0929@naver.com>
2025-08-20 15:31:04 +0900

    setting: docker compose 세팅 및 typeorm 패키지 설치

--- cf_sns/src/posts/posts.controller.ts
+++ cf_sns/src/posts/posts.controller.ts
@@ -1,48 +1,13 @@
 import {
   Body,
   Controller,
+  Delete,
   Get,
-  NotFoundException,
   Param,
   Post,
+  Put,
 } from '@nestjs/common';
 import { PostsService } from './posts.service';
-
-interface PostModel {
-  id: number;
-  author: string;
-  title: string;
-  content: string;
-  likeCount: number;
-  commentCount: number;
-}
-
-let posts: PostModel[] = [
-  {
-    id: 1,
-    author: 'newjeans_offical',
-    title: '뉴진스 민지',
-    content: '메이크업 고치고 있는 민지',
-    likeCount: 123456,
-    commentCount: 9999999,
-  },
-  {
-    id: 2,
-    author: 'newjeans_offical',
-    title: '뉴진스 해린',
-    content: '노래하고있는 해린',
-    likeCount: 123456,
-    commentCount: 9999999,
-  },
-  {
-    id: 3,
-    author: 'blackpink_offical',
-    title: '블랙핑크 로제',
-    content: '아파트아파트',
-    likeCount: 123456,
-    commentCount: 9999999,
-  },
-];
 
 @Controller('posts')
 export class PostsController {
@@ -52,20 +17,14 @@ export class PostsController {
   // 모든 post를 다 가져온다
   @Get()
   getPosts() {
-    return posts;
+    return this.postsService.getAllPosts();
   }
 
   // 2) GET /posts/:id
   // id에 해당되는 post를 가져온다
   @Get(':id')
   getPost(@Param('id') id: string) {
-    const post = posts.find((post) => post.id === +id);
-
-    if (!post) {
-      throw new NotFoundException();
-    }
-
-    return post;
+    return this.postsService.getPostById(+id);
   }
 
   // 3) POST /posts
@@ -76,23 +35,25 @@ export class PostsController {
     @Body('title') title: string,
     @Body('content') content: string,
   ) {
-    const post: PostModel = {
-      id: posts[posts.length - 1].id + 1,
-      author,
-      title,
-      content,
-      likeCount: 0,
-      commentCount: 0,
-    };
-
-    posts = [...posts, post];
-
-    return post;
+    return this.postsService.createPost(author, title, content);
   }
 
-  // 4) PUT /posts/:id
+  // 4) PATCH /posts/:id
   // id에 해당되는 post를 변경한다
+  @Put(':id')
+  putPost(
+    @Param('id') id: string,
+    @Body('author') author?: string,
+    @Body('title') title?: string,
+    @Body('content') content?: string,
+  ) {
+    return this.postsService.updatePost(+id, author, title, content);
+  }
 
   // 5) DELETE /posts/:id
   // id에 해당되는 post를 삭제한다
+  @Delete(':id')
+  deletePost(@Param('id') id: string) {
+    return this.postsService.deletePost(+id);
+  }
 }
